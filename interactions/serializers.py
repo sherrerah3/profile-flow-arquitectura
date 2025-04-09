8d86bcd811f92842963ccfb3a05df96e7454c44f
TeoVH <villadamateo64@gmail.com>
2025-04-09 09:41:54 -0500

    Agregar serializer para gestionar los likes de los usuarios sobre vacantes

---
+++ interactions/serializers.py
@@ -0,0 +1,19 @@
+# interactions/serializers.py
+
+from rest_framework import serializers
+from .models import Interaction
+from jobs.models import Job
+
+class JobMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Job
+        fields = ['id', 'title', 'company', 'location']
+
+class InteractionSerializer(serializers.ModelSerializer):
+    job = serializers.PrimaryKeyRelatedField(queryset=Job.objects.all())
+    job_details = JobMiniSerializer(source='job', read_only=True)
+
+    class Meta:
+        model = Interaction
+        fields = ['id', 'user', 'job', 'job_details', 'interaction_type', 'timestamp']
+        read_only_fields = ['user', 'timestamp']
