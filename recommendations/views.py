f2b7014b5abc4b09acff99f209c06357c3f1f99c
Samuel <sherrerah3@eafit.edu.co>
2025-05-23 12:40:10 -0500

    Modificacion en la vista de recomendaciones

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .services.tfidf_recommender import recomendar_vacantes
 
-# Create your views here.
+class JobRecommendationsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        recomendaciones = recomendar_vacantes(request.user)
+        data = [
+            {
+                "id": job.id,
+                "title": job.title,
+                "description": job.description,
+                "similarity": job.similarity,
+                "company": job.company,
+                "location": job.location,
+            }
+            for job in recomendaciones
+        ]
+        return Response(data)
