777803ede8a41770f724b092942062607e05a52d
TeoVH <villadamateo64@gmail.com>
2025-05-22 23:40:44 -0500

    Ajusta vista de recomendaciones para manejar errores y optimizar la lógica

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .services.tfidf_recommender import recomendar_vacantes
+from jobs.serializers import JobSerializer
+
+class JobRecommendationsView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        recomendaciones = recomendar_vacantes(user)
+        serializer = JobSerializer(recomendaciones, many=True)
+        return Response(serializer.data)
