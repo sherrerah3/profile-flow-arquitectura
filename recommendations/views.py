a614f0cdbc354a316db9a7f3afb68409715653dd
TeoVH <villadamateo64@gmail.com>
2025-05-23 09:25:27 -0500

    Actualizar la lógica en las vistas de recomendaciones

--- recommendations/views.py
+++ recommendations/views.py
@@ -1,15 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-
 from .services.tfidf_recommender import recomendar_vacantes
-from jobs.serializers import JobSerializer
 
 class JobRecommendationsView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
-        user = request.user
-        recomendaciones = recomendar_vacantes(user)
-        serializer = JobSerializer(recomendaciones, many=True)
-        return Response(serializer.data)
+        recomendaciones = recomendar_vacantes(request.user)
+        data = [
+            {
+                "id": job.id,
+                "title": job.title,
+                "description": job.description,
+                "similarity": job.similarity,
+            }
+            for job in recomendaciones
+        ]
+        return Response(data)
